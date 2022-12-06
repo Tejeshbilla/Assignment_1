4c284e96de745b07d0ab399e5a15b166eaebf390
Tejeshbilla <105988170+Tejeshbilla@users.noreply.github.com>
2022-12-07 00:56:33 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
-import functions
+import Functions
 
 log_reg = int(input("If you are an existing user please lggin else Register. \nPress: \n\t1 - To log in \n\t2 - to Register\n\t"))
 if log_reg == 1:
-    functions.login()
+    Functions.login()
 elif log_reg == 2:
-    functions.register()
+    Functions.register()
